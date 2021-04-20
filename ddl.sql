ea733b0c6f4d5a40f2c568f7fa29df578150f596
James Rooney <jr1457@wildcats.unh.edu>
2021-04-20 18:41:10 -0400

    Created patriot table and edited location in away and home tables

--- ddl.sql
+++ ddl.sql
@@ -1,10 +1,13 @@
 DROP DATABASE IF EXISTS comp520project1;
 CREATE DATABASE comp520project1;
 USE comp520project1;
+CREATE TABLE patriots(
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+);
 CREATE TABLE awayGames(
     awayGame_num INT(11) PRIMARY KEY AUTO_INCREMENT,
     game_score INT(11) NOT NULL,
-    location VARCHAR(255) NOT NULL,
+    "location" VARCHAR(255) NOT NULL,
     stadium VARCHAR(255) NOT NULL,
     opp_team_location VARCHAR(255) NOT NULL,
     opp_team_nick VARCHAR(255) NOT NULL
@@ -12,7 +15,7 @@ CREATE TABLE awayGames(
 CREATE TABLE homeGames(
     homeGame_num INT(11) PRIMARY KEY AUTO_INCREMENT,
     game_score INT(11) NOT NULL,
-    location VARCHAR(255) NOT NULL,
+    "location" VARCHAR(255) NOT NULL,
     stadium VARCHAR(255) NOT NULL,
     opp_team_location VARCHAR(255) NOT NULL,
     opp_team_nick VARCHAR(255) NOT NULL
