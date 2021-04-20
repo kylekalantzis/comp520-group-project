0dfc1db5d1ee7364fa4c7ff02da019c9cef08fb7
James Rooney <jr1457@wildcats.unh.edu>
2021-04-20 18:32:44 -0400

    Created home and away game tables

--- ddl.sql
+++ ddl.sql
@@ -1,3 +1,20 @@
 DROP DATABASE IF EXISTS comp520project1;
 CREATE DATABASE comp520project1;
 USE comp520project1;
+CREATE TABLE awayGames(
+    awayGame_num INT(11) PRIMARY KEY AUTO_INCREMENT,
+    game_score INT(11) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    stadium VARCHAR(255) NOT NULL,
+    opp_team_location VARCHAR(255) NOT NULL,
+    opp_team_nick VARCHAR(255) NOT NULL
+);
+CREATE TABLE homeGames(
+    homeGame_num INT(11) PRIMARY KEY AUTO_INCREMENT,
+    game_score INT(11) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    stadium VARCHAR(255) NOT NULL,
+    opp_team_location VARCHAR(255) NOT NULL,
+    opp_team_nick VARCHAR(255) NOT NULL
+);
+
