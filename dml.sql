eda5551bf02a871f0db428ed75ae0f56b04c4f33
Kyle Kalantzis <kmk1052@wildcats.unh.edu>
2021-04-21 07:35:48 -0400

    Added values to awayGames

--- dml.sql
+++ dml.sql
@@ -9,3 +9,17 @@ VALUES
 ('24', '10', 'Foxborough, Massachusetts', 'Gillette Stadium', 'Buffalo, New York', 'The Electric Company'),
 ('38', '3', 'Foxborough, Massachusetts', 'Gillette Stadium', 'East Rutherford, New Jersey', 'Green Gang'),
 ('41', '28', 'Foxborough, Massachusetts', 'Gillette Stadium', 'Los Angeles, California', 'The Bolts');
+
+INSERT INTO awayGames (pats_score, opp_score, location, stadium, opp_team_location, opp_team_nick)
+VALUES
+('20', '31', 'Jacksonville, Florida', 'TIAA Bank Stadium', 'Jacksonville, Florida', 'Jags'),
+('10', '26', 'Detroit, Michigan', 'Ford Field', 'Detroit, Michigan', 'Silver Crush'),
+('38', '31', 'Chicago, Illinois', 'Soldier Field', 'Chicago, Illinois', 'Da Bears'),
+('25', '6', 'Buffalo, New York', 'New Era Field', 'Buffalo, New York', 'The Electric Company'),
+('10', '34', 'Nashville, Tennessee', 'Nissan Stadium', 'Nashville, Tennessee', 'Smash and Dash'),
+('27', '13', 'East Rutherford, New Jersey', 'MetLife Stadium', 'East Rutherford, New Jersey', 'Green Gang'),
+('33', '34', 'Miami, Florida', 'Hard Rock Stadium', 'Miami, Florida', 'The Phins'),
+('10', '17', 'Pittsburgh, Pennsylvania', 'Heinz Field', 'Pittsburgh, Pennsylvania', 'Blitzburgh'),
+('37', '31', 'Kansas City, Missouri', 'Arrowhead Stadium', 'Kansas City, Missouri', 'Chiefs Nation'),
+('13', '3', 'Atlanta, Georgia', 'Mercedes-Benz Stadium', 'Atlanta, Georgia', 'Greatest Show on Turf');
+
