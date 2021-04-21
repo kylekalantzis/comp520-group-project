f7881cc8a3416d7ef38ff2503c8b5f9383920c94
James Rooney <jr1457@wildcats.unh.edu>
2021-04-20 21:38:16 -0400

    added first home game data

--- dml.sql
+++ dml.sql
@@ -1,3 +1,6 @@
 -- Insert code here
 -- This is the line comment to show how Git works
 -- Test from james
+INSERT INTO homeGames (pats_score, opp_score, location, stadium, opp_team_location, opp_team_nick)
+VALUES
+('27', '20', 'Foxborough, Massachusetts', 'Gillette Stadium', 'Houston, Texas', 'Bulls on Parade');
