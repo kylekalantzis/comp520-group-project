e26300ee24768a68a1dd8ddb9af2ba6cfce57852
Kyle Kalantzis <kmk1052@wildcats.unh.edu>
2021-04-21 07:11:58 -0400

    Added values to table homeGames

--- dml.sql
+++ dml.sql
@@ -1,3 +1,11 @@
--- Insert code here
--- This is the line comment to show how Git works
--- Test from james
+INSERT INTO homeGames (pats_score, opp_score, location, stadium, opp_team_location, opp_team_nick)
+VALUES
+('27', '20', 'Foxborough, Massachusetts', 'Gillette Stadium', 'Houston, Texas', 'Bulls on Parade'),
+('38', '7', 'Foxborough, Massachusetts', 'Gillette Stadium', 'Miami, Florida', 'The Phins' ),
+('38', '24', 'Foxborough, Massachusetts', 'Gillette Stadium', 'Indianapolis, Indiana', 'Dolts'),
+('43', '40', 'Foxborough, Massachusetts', 'Gillette Stadium', 'Kansas City, Missouri', 'Chiefs Nation'),
+('31', '17', 'Foxborough, Massachusetts', 'Gillette Stadium', 'Green Bay, Wisconsin', 'The Pack Attack'),
+('24', '10', 'Foxborough, Massachusetts', 'Gillette Stadium', 'Minneapolis, Minnesota', 'Purple People Eaters'),
+('24', '10', 'Foxborough, Massachusetts', 'Gillette Stadium', 'Buffalo, New York', 'The Electric Company'),
+('38', '3', 'Foxborough, Massachusetts', 'Gillette Stadium', 'East Rutherford, New Jersey', 'Green Gang'),
+('41', '28', 'Foxborough, Massachusetts', 'Gillette Stadium', 'Los Angeles, California', 'The Bolts');
